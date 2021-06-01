f83a6196600e4ffbd063730015eaa62ee027af14
Thaisa <ta_lemos@hotmail.com>
2021-05-31 23:10:14 -0300

    Frescuras de lint

--- rps.js
+++ rps.js
@@ -2,38 +2,38 @@ function computerPlay()
 {
     const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choices.length);
-    console.log (choices[random]);
     return choices[random];
 }
+
 function playerPlay()
 {
     let answer = prompt("What is your choice: Rock, Paper or Scissors?").toLowerCase();    
     return answer;
-    console.log (answer);
 }
+
 function playRound()
 { 
     let computerChoice = computerPlay();
     let playerChoice = playerPlay();
+    let result;
     if(computerChoice === playerChoice) {
         result = "It's a tie";
     }
-        else if (computerChoice === "rock")
-        {
-                if (playerChoice === "paper"){
-                result = "You win";
-                }
-                else {
-                result = "You lose";
-                }
-            }
-        else if (computerChoice === "paper") {
-                if (playerChoice === "scissors"){
-                result = "You win";
-                }
-                else {
-                result = "You lose";
-                }
+    else if (computerChoice === "rock") {
+        if (playerChoice === "paper") {
+            result = "You win";
+        }
+        else {
+            result = "You lose";
+        }
+    }
+    else if (computerChoice === "paper") {
+        if (playerChoice === "scissors") {
+            result = "You win";
+        }
+        else {
+            result = "You lose";
+        }
     }
     else {
         if(playerChoice === "rock") {
@@ -42,45 +42,34 @@ function playRound()
         else {
             result = "You lose";
         }
-    
-        
-
     }
    return result;
 }
+
 function game() {
     let playerScore = 0;
     let computerScore = 0;
     let round = 1;
+    let roundResult;
     while (round <=5 ){
-        playRound();
+        roundResult = playRound();
         round++;
-        if (result === "You win") {
-            playerScore++;
-            console.log (result);
-            console.log (playerScore);
-        }
-        else if (result === "It's a tie"){
-            console.log (result);
-            console.log (playerScore);
-        }
-        else {
-            console.log (result);
-            console.log (playerScore);
-            computerScore++;
-        } 
-        }
-    console.log (playerScore);
-        if (playerScore > computerScore) {
+        if (roundResult === "You win") playerScore++;    
+        if (roundResult === "You lose") computerScore++;
+    }
+
+    if (playerScore > computerScore) {
             finalResult = "Wow, you won!!";
-        }
-        else if (playerScore === computerScore) {
+    }
+    else if (playerScore === computerScore) {
             finalResult = "It's a tie!";
-        }
-        else {
-            finalResult = "Oops, you lost... try again!";
-        }
+    }
+    else {
+        finalResult = "Oops, you lost... try again!";
+    }
+
 return finalResult;
 }
     
 
+game();
